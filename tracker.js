ce9dbf4e02c7413f17a6ac0b3e1474205caef0bb
0dsabella <42749703+0dsabella@users.noreply.github.com>
2018-11-15 13:55:59 -0600

    Add files via upload

--- tracker.js
+++ tracker.js
@@ -5,15 +5,35 @@ function Tracker(){
   let percent = [];
 
   //implement all six functions below
-function numOfSeedslist(value){
+function numOfSeeds(list){
   let found = 0;
-  for(let i=null; i<list.length; i++){
-    if(list[i]===value){
+  for(let i=0; i<list.length; i++){
+    if(list[i] !== null){
       found++;
     }
   }
 }
-
-
-
+function addSeed(s){
+  list.push(s);
+  return list;
+}
+function removeSeed(list){
+  function findValue(name){
+    for(let i=0; i<list.length; i++){
+      if(list[i]===name){
+        return null
+      }
+    }
+  }
+}
+function reportHealth(){
+  function average(list){
+    let sum=0;
+    for(let i=0; i<list.length; i++){
+      sum=sum+list[i];
+    }
+    return sum/list.length;
+    }
+  }
+}
 }
