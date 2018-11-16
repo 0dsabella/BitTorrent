0d36309b90eaee47f611c33b6b7ab51f0d179887
0dsabella <42749703+0dsabella@users.noreply.github.com>
2018-11-16 13:55:32 -0600

    Add files via upload

--- tracker.js
+++ tracker.js
@@ -17,23 +17,27 @@ function addSeed(s){
   list.push(s);
   return list;
 }
-function removeSeed(list){
-  function findValue(name){
+function removeSeed(list, name){
     for(let i=0; i<list.length; i++){
       if(list[i]===name){
         return null
       }
     }
   }
-}
-function reportHealth(){
-  function average(list){
+function reportHealth(list){
     let sum=0;
     for(let i=0; i<list.length; i++){
+      if(list[i]!=0){
       sum=sum+list[i];
+      }
     }
     return sum/list.length;
-    }
+    
   }
 }
+function update(){
+  Math.random(0, 1);
+  for(let i=0; i<list.length; i++){
+  
+  
 }
