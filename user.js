4d1934a1d0dadefce5faa2385daeaaf55858e59b
0dsabella <42749703+0dsabella@users.noreply.github.com>
2018-11-29 13:56:01 -0600

    Add files via upload

--- user.js
+++ user.js
@@ -3,11 +3,33 @@ function User(tracker, torrent){
 
   let myTracker = tracker;
   let myTorrent = torrent;
-  let torrentFile = [];
-  let torrentTrackerFile =;
-  let state =
+  let state = "seed";
+  let downloadSpeed = 0;
+  let uploadSpeed = 0;
+  
+  function startDownload(){
 
+  } 
 
+  function updateTracker(){
+   function Tracker(){
+   }
+  }
+  
+  function checkProgress()
+    function average(list){
+      for(let i=0; i < list.length; i++){
+        sum=sum+list[i];
+      }
+    return sum/list.length;
+    }
+  function leech(){
+    downloadSpeed=Math.random();
+    return downloadSpeed;
+  }
+
+  
+  function seed()
 
 
   //Type all of your code in here.
